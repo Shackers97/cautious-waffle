00a1f46411368f06392f0385c1f44b9124b24129
Andy Shackleton <andrew.shackleton1@ntlworld.com>
2018-12-14 23:27:44 +0000

    14/12/18

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,19 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(500, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(288, 404);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
             // 
             // button1
             // 
@@ -50,6 +42,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Enter";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_click);
             // 
             // button2
             // 
@@ -59,6 +52,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_click);
             // 
             // panel1
             // 
@@ -67,27 +61,35 @@
             this.panel1.Size = new System.Drawing.Size(469, 403);
             this.panel1.TabIndex = 3;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(500, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 403);
+            this.textBox1.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,105 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        int Cx, Cy;
+        int count = 0;
+        Regex regex = new Regex(@"\d+");
+
+        private void button1_click(object sender, EventArgs e)
+        {
+            Pen p = new Pen(Color.Red);
+            string test = textBox1.Text;
+            string[] cmdLine = test.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("array length: " + cmdLine.Length);
+
+            while (count < cmdLine.Length)
+            {
+                if (cmdLine[count].StartsWith("move"))
+                {
+                    Match match = regex.Match(cmdLine[count]);
+                    if (match.Success)
+                    {
+                        Console.WriteLine(match.Value);
+                        //update pen coordinates
+                    }
+                }
+                else if (cmdLine[count].StartsWith("up")) //PEN UP
+                {
+                    Match match = regex.Match(cmdLine[count]);
+                    if (match.Success)
+                    {
+                        int newY = Int32.Parse(match.Value) + Cy;
+                        panel1.CreateGraphics().DrawLine(p, Cx, Cy, Cx, -newY);
+                        Cy = -newY;
+                        Console.WriteLine("Val: " + match.Value +
+                            " Cx: " + Cx +
+                            " Cy: " + Cy);
+                    }
+                    else
+                        Console.WriteLine("Wrong Input");
+                }
+                else if (cmdLine[count].StartsWith("down")) //PEN DOWN
+                {
+                    Match match = regex.Match(cmdLine[count]);
+                    if (match.Success)
+                    {
+                        int newY = Int32.Parse(match.Value) + Cy;
+                        panel1.CreateGraphics().DrawLine(p, Cx, Cy, Cx, newY);
+                        Cy = newY;
+                        Console.WriteLine("Val: " + match.Value +
+                            " Cx: " + Cx +
+                            " Cy: " + Cy);
+                    }
+                    else
+                        Console.WriteLine("Wrong Input");
+                }
+                else if (cmdLine[count].StartsWith("right")) //PEN RIGHT
+                {
+                    Match match = regex.Match(cmdLine[count]);
+                    if (match.Success)
+                    {
+                        int newX = Int32.Parse(match.Value) + Cx;
+                        panel1.CreateGraphics().DrawLine(p, Cx, Cy, newX, Cy);
+                        Cx = newX;
+                        Console.WriteLine(match.Value + " Pen x: " + Cx + " Pen y: " + Cy);
+                    }
+                }
+                else if (cmdLine[count].StartsWith("left")) //PEN LEFT
+                {
+                    Match match = regex.Match(cmdLine[count]);
+                    if (match.Success)
+                    {
+                        int newX = Int32.Parse(match.Value) + Cx;
+                        panel1.CreateGraphics().DrawLine(p, Cx, Cy, -newX, Cy);
+                        Cx = -newX;
+                        Console.WriteLine(match.Value + " Pen x: " + Cx + " Pen y: " + Cy);
+                    }
+                }
+                else if (cmdLine[count].StartsWith("movePen"))
+                {
+                    Regex pattern = new Regex(@"\d+\s\d+");
+                    Match testpat = pattern.Match(cmdLine[count]);
+                    if (testpat.Success)
+                    {
+                        Console.WriteLine(testpat.Value);
+                    }
+                    else
+                        Console.WriteLine("false");
+                }
+                count++;
+            } 
+        }
+
+        private void button2_click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            count = 0;
+        }
     }
 }
+
+    internal class TextRange
+    {
+
+    }
